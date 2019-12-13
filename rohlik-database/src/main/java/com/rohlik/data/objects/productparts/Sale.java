4f8e87aca35734eaf9218bb8d9db023f9e480b1b
RadimP <36049310+RadimP@users.noreply.github.com>
2019-12-13 19:54:47 +0100

    Added methods for conversion to Sale class

--- rohlik-database/src/main/java/com/rohlik/data/objects/productparts/Sale.java
+++ rohlik-database/src/main/java/com/rohlik/data/objects/productparts/Sale.java
@@ -8,67 +8,96 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-    "id",
-    "type",
-    "promoted",
-    "remaining",
-    "price",
-    "priceForUnit",
-    "originalPrice",
-    "discountPercentage",
-    "discountPrice",
-    "multipackNeedAmount",
-    "multipackAsPriceDiff",
-    "asPriceDiff",
-    "startedAt",
-    "endsAt",
-    "originalSemiCaliberPricePerKg",
-    "codeActivationId",
-    "active",
-    "soonExpirationType",
-    "soonExpirationTypeId"
-})
+@JsonPropertyOrder({ "id", "type", "promoted", "remaining", "price", "priceForUnit", "originalPrice",
+		"discountPercentage", "discountPrice", "multipackNeedAmount", "multipackAsPriceDiff", "asPriceDiff",
+		"startedAt", "endsAt", "originalSemiCaliberPricePerKg", "codeActivationId", "active", "soonExpirationType",
+		"soonExpirationTypeId" })
 @Data
 public class Sale {
 
-    @JsonProperty("id")
-    public Integer id;
-    @JsonProperty("type")
-    public String type;
-    @JsonProperty("promoted")
-    public Boolean promoted;
-    @JsonProperty("remaining")
-    public Integer remaining;
-    @JsonProperty("price")
-    public Price price;
-    @JsonProperty("priceForUnit")
-    public PriceForUnit priceForUnit;
-    @JsonProperty("originalPrice")
-    public OriginalPrice originalPrice;
-    @JsonProperty("discountPercentage")
-    public Integer discountPercentage;
-    @JsonProperty("discountPrice")
-    public DiscountPrice discountPrice;
-    @JsonProperty("multipackNeedAmount")
-    public Object multipackNeedAmount;
-    @JsonProperty("multipackAsPriceDiff")
-    public Boolean multipackAsPriceDiff;
-    @JsonProperty("asPriceDiff")
-    public Boolean asPriceDiff;
-    @JsonProperty("startedAt")
-    public Integer startedAt;
-    @JsonProperty("endsAt")
-    public Integer endsAt;
-    @JsonProperty("originalSemiCaliberPricePerKg")
-    public String originalSemiCaliberPricePerKg;
-    @JsonProperty("codeActivationId")
-    public Object codeActivationId;
-    @JsonProperty("active")
-    public Boolean active;
-    @JsonProperty("soonExpirationType")
-    public String soonExpirationType;
-    @JsonProperty("soonExpirationTypeId")
-    public Integer soonExpirationTypeId;
+	@JsonProperty("id")
+	private Integer id;
+	@JsonProperty("type")
+	private String type;
+	@JsonProperty("promoted")
+	private Boolean promoted;
+	@JsonProperty("remaining")
+	private Integer remaining;
+	@JsonProperty("price")
+	private Price price;
+	@JsonProperty("priceForUnit")
+	private PriceForUnit priceForUnit;
+	@JsonProperty("originalPrice")
+	private OriginalPrice originalPrice;
+	@JsonProperty("discountPercentage")
+	private Integer discountPercentage;
+	@JsonProperty("discountPrice")
+	private DiscountPrice discountPrice;
+	@JsonProperty("multipackNeedAmount")
+	private Object multipackNeedAmount;
+	@JsonProperty("multipackAsPriceDiff")
+	private Boolean multipackAsPriceDiff;
+	@JsonProperty("asPriceDiff")
+	private Boolean asPriceDiff;
+	@JsonProperty("startedAt")
+	private Integer startedAt;
+	@JsonProperty("endsAt")
+	private Integer endsAt;
+	@JsonProperty("originalSemiCaliberPricePerKg")
+	private String originalSemiCaliberPricePerKg;
+	@JsonProperty("codeActivationId")
+	private Object codeActivationId;
+	@JsonProperty("active")
+	private Boolean active;
+	@JsonProperty("soonExpirationType")
+	private String soonExpirationType;
+	@JsonProperty("soonExpirationTypeId")
+	private Integer soonExpirationTypeId;
 
+	public com.rohlik.data.entities.Sale toSale() {
+		com.rohlik.data.entities.Sale sale = new com.rohlik.data.entities.Sale();
+		sale.setType(this.getType());
+		sale.setDiscountPercentage(this.getDiscountPercentageAsDouble());
+		sale.setDiscountPrice(this.getFullDiscountPrice());
+		sale.setOriginalPrice(this.getFullOriginalPrice());
+		sale.setPrice(getFullPrice());
+		sale.setPricePerUnit(this.getFullPriceForUnit());
+		sale.setId(this.getId());
+		sale.setPromoted(this.getPromoted());
+		return sale;
+	}
+
+	public Double getDiscountPercentageAsDouble() {
+		if (this.getDiscountPercentage() != null)
+			return this.getDiscountPercentage().doubleValue();
+		return null;
+	}
+
+	public Double getFullDiscountPrice() {
+		if (this.getDiscountPrice() != null && this.getDiscountPrice().getFull() != null) {
+			return this.getDiscountPrice().getFull().doubleValue();
+		}
+		return null;
+	}
+
+	public Double getFullOriginalPrice() {
+		if (this.getOriginalPrice() != null && this.getOriginalPrice().getFull() != null) {
+			return this.getOriginalPrice().getFull().doubleValue();
+		}
+		return null;
+	}
+
+	public Double getFullPrice() {
+		if (this.getPrice() != null && this.getPrice().getFull() != null) {
+			return this.getPrice().getFull().doubleValue();
+		}
+		return null;
+	}
+
+	public Double getFullPriceForUnit() {
+		if (this.getPriceForUnit() != null && this.getPriceForUnit().getFull() != null) {
+			return this.getPriceForUnit().getFull().doubleValue();
+		}
+		return null;
+	}
 }
