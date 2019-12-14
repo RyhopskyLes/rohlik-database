a481fcca6abb8c642a88f05a7712589b4cf1c971
RadimP <36049310+RadimP@users.noreply.github.com>
2019-12-14 10:54:46 +0100

    Added dependencies and methods for creating list of products with producers in ProductsInCategory class

--- rohlik-database/src/main/java/com/rohlik/data/objects/ProductsInCategory.java
+++ rohlik-database/src/main/java/com/rohlik/data/objects/ProductsInCategory.java
@@ -1,9 +1,13 @@
 package com.rohlik.data.objects;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
 import java.util.Spliterator;
 import java.util.stream.StreamSupport;
 
@@ -19,13 +23,18 @@ import com.rohlik.data.entities.Product;
 @Component("ProductsInCategory")
 public class ProductsInCategory {
 	private RootObject rootObject;
- private static final String PRODUCT_LIST = "productList";
- private static final String DATA = "data";
- private static final String PRODUCT_ID = "productId";
- private static final String TOTAL_HITS = "totalHits";
+	private Filters filters;
+	private Navigation navigation;
+	private static final String PRODUCT_LIST = "productList";
+	private static final String DATA = "data";
+	private static final String PRODUCT_ID = "productId";
+	private static final String TOTAL_HITS = "totalHits";
+
 	@Autowired
-	public ProductsInCategory(RootObject rootObject) {
-		this.rootObject=rootObject;
+	public ProductsInCategory(RootObject rootObject, Filters filters, Navigation navigation) {
+		this.rootObject = rootObject;
+		this.filters = filters;
+		this.navigation=navigation;
 	}
 
 	public List<RawProduct> getRawProductListForCategory(Integer categoryId, Integer limitResults) {
@@ -41,7 +50,7 @@ public class ProductsInCategory {
 
 		return products;
 	}
-	
+
 	public List<Product> getProductListForCategory(Integer categoryId, Integer limitResults) {
 		Gson gson = new Gson();
 		List<Product> products = new ArrayList<>();
@@ -49,13 +58,30 @@ public class ProductsInCategory {
 		Optional<JsonArray> productList = data.map(theData -> theData.getAsJsonArray(PRODUCT_LIST));
 		Spliterator<JsonElement> elements = productList.orElseGet(JsonArray::new).spliterator();
 		StreamSupport.stream(elements, false).forEach(product -> {
-			RawProduct converted = gson.fromJson(product, RawProduct.class);			
+			RawProduct converted = gson.fromJson(product, RawProduct.class);
 			products.add(converted.toProduct());
 		});
 
 		return products;
 	}
-	
+
+	public List<Product> getProductListForCategoryWithProducers(Integer categoryId, Integer limitResults) {
+		Gson gson = new Gson();
+		List<Product> products = new ArrayList<>();
+		Map<String, Set<Integer>> producers = producersWithProductsForCategory(categoryId);
+		Optional<JsonObject> data = getJsonDataObject(categoryId, limitResults);
+		Optional<JsonArray> productList = data.map(theData -> theData.getAsJsonArray(PRODUCT_LIST));
+		Spliterator<JsonElement> elements = productList.orElseGet(JsonArray::new).spliterator();
+		StreamSupport.stream(elements, false).forEach(product -> {
+			RawProduct converted = gson.fromJson(product, RawProduct.class);
+			Product productConverted = converted.toProduct();
+			setProducerName(productConverted, producers);
+			products.add(productConverted);
+		});
+
+		return products;
+	}
+
 	public List<Product> getProductListForCategoryWithSales(Integer categoryId, Integer limitResults) {
 		Gson gson = new Gson();
 		List<Product> products = new ArrayList<>();
@@ -63,12 +89,12 @@ public class ProductsInCategory {
 		Optional<JsonArray> productList = data.map(theData -> theData.getAsJsonArray(PRODUCT_LIST));
 		Spliterator<JsonElement> elements = productList.orElseGet(JsonArray::new).spliterator();
 		StreamSupport.stream(elements, false).forEach(product -> {
-			RawProduct converted = gson.fromJson(product, RawProduct.class);			
+			RawProduct converted = gson.fromJson(product, RawProduct.class);
 			products.add(converted.toProductWithSales());
 		});
 		return products;
 	}
-	
+
 	public Optional<JsonArray> getProductListJsonArrayForCategory(Integer categoryId, Integer limitResults) {
 		Optional<JsonObject> data = getJsonDataObject(categoryId, limitResults);
 		return data.map(theData -> theData.getAsJsonArray(PRODUCT_LIST));
@@ -81,22 +107,60 @@ public class ProductsInCategory {
 
 		Spliterator<JsonElement> elements = productList.orElseGet(JsonArray::new).spliterator();
 		StreamSupport.stream(elements, false).map(Optional::ofNullable).forEach(productElement -> {
-			Integer productId = productElement.map(JsonElement::getAsJsonObject)
-					.map(object -> object.get(PRODUCT_ID)).map(JsonElement::getAsInt).orElse(null);
-			if(productId!=null) products.add(productId);
+			Integer productId = productElement.map(JsonElement::getAsJsonObject).map(object -> object.get(PRODUCT_ID))
+					.map(JsonElement::getAsInt).orElse(null);
+			if (productId != null)
+				products.add(productId);
 		});
 		return products;
 	}
-	
+
 	private Optional<JsonObject> getJsonDataObject(Integer categoryId, Integer limitResults) {
-		return Objects.equals(rootObject.getCategoryId(), categoryId)&&Objects.equals(rootObject.getLimitResults(), limitResults)
-				&& Objects.equals(rootObject.getFrom(), RootObject.CalledFrom.CATEGORYDATA) ? rootObject.getJsonObject().map(object -> object.getAsJsonObject(DATA))
-				:rootObject.dataForCategory(categoryId, limitResults).map(object -> object.getAsJsonObject(DATA));
+		return Objects.equals(rootObject.getCategoryId(), categoryId)
+				&& Objects.equals(rootObject.getLimitResults(), limitResults)
+				&& Objects.equals(rootObject.getFrom(), RootObject.CalledFrom.CATEGORYDATA)
+						? rootObject.getJsonObject().map(object -> object.getAsJsonObject(DATA))
+						: rootObject.dataForCategory(categoryId, limitResults)
+								.map(object -> object.getAsJsonObject(DATA));
 	}
-	
-	public Optional<String> getTotalHitsForCategory(Integer number) {
-		return rootObject.dataForCategory(number, 25)
+
+	public Optional<String> getTotalHitsForCategory(Integer categoryId) {
+		return rootObject.dataForCategory(categoryId, 25)
 				.map(object -> object.getAsJsonObject(DATA).get(TOTAL_HITS).getAsString());
+	}
+
+	private Map<String, Set<Integer>> producersWithProductsForCategory(Integer categoryId) {
+		List<SlugAndName> slugMap = filters.forCategoryAndSlug(categoryId, "znacka");
+		return producersWithProducts(slugMap, categoryId);
+	}
+
+	private Map<String, Set<Integer>> producersWithProducts(List<SlugAndName> slugsAndNames, Integer categoryId) {
+		Map<String, Set<Integer>> productMap = new HashMap<>();
+		String producer = "";
+		for (SlugAndName slugAndName : slugsAndNames) {
+			Optional<JsonObject> data = rootObject.dataForCategoryAndProducer(categoryId, slugAndName.getSlug())
+					.map(object -> object.getAsJsonObject(DATA));
+			Optional<JsonArray> productList = data.map(object -> object.get(PRODUCT_LIST).getAsJsonArray());
+			producer = slugAndName.getName();
+			if (productList.isPresent()) {
+				Set<Integer> products = new HashSet<>();
+				for (int i = 0; i < productList.get().size(); i++) {
+					JsonObject descriptionData = productList.get().get(i).getAsJsonObject();
+					Integer product = descriptionData.get(PRODUCT_ID).getAsInt();
+					products.add(product);
+				}
+				productMap.put(producer, products);
+			}
+		}
+		return productMap;
+	}
+
+	private void setProducerName(Product product, Map<String, Set<Integer>> producers) {
+		Integer productId = product.getProductId();
+		product.setProducer("");
+		producers.entrySet().stream().filter(entry -> entry.getValue().contains(productId))
+				.forEach(entry -> product.setProducer(entry.getKey()));
+
 	}
 
 }
