e5ada64c98dbf0fe54b4efefc0767d7fd2a32e66
RadimP <36049310+RadimP@users.noreply.github.com>
2020-01-02 11:17:38 +0100

    Minor change in DataConfig class

--- rohlik-database/src/main/java/com/rohlik/data/commons/config/DataConfig.java
+++ rohlik-database/src/main/java/com/rohlik/data/commons/config/DataConfig.java
@@ -64,7 +64,7 @@ public class DataConfig {
 	@Bean
 	public Properties hibernateProperties() {
 		Properties hibernateProp = new Properties();
-		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+		hibernateProp.put("hibernate.dialect", "org.hibernate.dialect.MySQL57Dialect");
 		hibernateProp.put("hibernate.format_sql", true);
 		hibernateProp.put("hibernate.use_sql_comments", true);
 		hibernateProp.put("hibernate.show_sql", true);
