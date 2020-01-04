b18c3665d049583dd6b079d30b9b2ef94a00d415
RadimP <36049310+RadimP@users.noreply.github.com>
2020-01-04 15:30:30 +0100

    Added bean configuration with catched exception in AppConfig and AppEmptyDBConfig classes

--- rohlik-database/src/main/java/com/rohlik/data/commons/config/AppConfig.java
+++ rohlik-database/src/main/java/com/rohlik/data/commons/config/AppConfig.java
@@ -1,8 +1,17 @@
 package com.rohlik.data.commons.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+
+import com.rohlik.data.commons.exceptions.WrongCategoryIdException;
+import com.rohlik.data.commons.services.build.CategoryBuildService;
+import com.rohlik.data.entities.Category;
+import com.rohlik.data.objects.CategorySnapshot;
+import com.rohlik.data.objects.ProductsInCategory;
 
 /**
  * Created by iuliana.cosmina on 4/29/17.
@@ -11,8 +20,21 @@ import org.springframework.context.annotation.Profile;
 @Profile("production")
 @ComponentScan(basePackages = {"com.rohlik.data"})
 public class AppConfig {
-	
-	
+	@Autowired
+	private ProductsInCategory productsInCategory;
+	@Autowired
+	private CategoryBuildService buildService;
+	@Bean
+	   @Scope(value = "prototype")
+	   public CategorySnapshot snapshot(Category category) {
+		CategorySnapshot snapshot;
+	       try {
+			snapshot = new CategorySnapshot(productsInCategory, buildService, category);
+		} catch (WrongCategoryIdException e) {
+			snapshot = new CategorySnapshot();
+			e.printStackTrace();
+		}
+	       return snapshot;}
 	
 }
 
--- rohlik-database/src/test/java/com/rohlik/data/config/AppEmptyDBConfig.java
+++ rohlik-database/src/test/java/com/rohlik/data/config/AppEmptyDBConfig.java
@@ -1,16 +1,38 @@
 package com.rohlik.data.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Scope;
+
+import com.rohlik.data.commons.exceptions.WrongCategoryIdException;
+import com.rohlik.data.commons.services.build.CategoryBuildService;
+import com.rohlik.data.entities.Category;
+import com.rohlik.data.objects.CategorySnapshot;
+import com.rohlik.data.objects.ProductsInCategory;
 
 @Configuration
 @Profile("emptyDB")
 
 @ComponentScan(basePackages = {"com.rohlik.data"})
 public class AppEmptyDBConfig {
-	
-	
-	
+	@Autowired
+	private ProductsInCategory productsInCategory;
+	@Autowired
+	private CategoryBuildService buildService;
+	@Bean
+	   @Scope(value = "prototype")
+	   public CategorySnapshot snapshot(Category category) {
+		CategorySnapshot snapshot;
+	       try {
+			snapshot = new CategorySnapshot(productsInCategory, buildService, category);
+		} catch (WrongCategoryIdException e) {
+			snapshot = new CategorySnapshot();
+			e.printStackTrace();
+		}
+	       return snapshot;
+	   }
 	
 }
