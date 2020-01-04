41e70c686a5b2731b435165ccf6d8a5b59a092c2
RadimP <36049310+RadimP@users.noreply.github.com>
2020-01-04 14:49:36 +0100

    Custom exception thrown in Breadcrumb class.

--- rohlik-database/src/main/java/com/rohlik/data/objects/Breadcrumb.java
+++ rohlik-database/src/main/java/com/rohlik/data/objects/Breadcrumb.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.apache.commons.lang3.math.NumberUtils;
 
+import com.rohlik.data.commons.exceptions.WrongCategoryIdException;
+
 import lombok.Data;
 
 @Data
@@ -12,12 +14,15 @@ private String title;
 private String link;
 private List<Breadcrumb> children;
 
-public Integer getCategoryId() {
+public Integer getCategoryId() throws WrongCategoryIdException {
+Integer id=null;	
+if(!link.isEmpty()&&link.length()>1)	{
 int lastSlashIndex = this.link.lastIndexOf('/');
 String linkEnd = this.link.substring(lastSlashIndex+1);
 int firstHyphenIndex= linkEnd.indexOf('-');
 String categoryId = linkEnd.substring(1, firstHyphenIndex);
-return NumberUtils.toInt(categoryId);	
+id= NumberUtils.toInt(categoryId);	} else {throw new WrongCategoryIdException("Root link "+link+" and title "+title+" in breadcrumb.\r\n Probably used wrong categoryId in url"); }
+return id;
 }
 
 }
